2d96e8a628b879e95f287b7005e021a0a627d9fb
UltimaVanguard <ultimavanguard@gmail.com>
2024-04-20 11:43:39 -0500

    added needed packages, questions array, inquirer prompt, and started the .then of inquirer

--- index.js
+++ index.js
@@ -1,13 +1,74 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const markdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+     },
+     {
+        type: 'input',
+        message: 'What is the description?',
+        name: 'description',
+     },
+     {
+        type: 'input',
+        message: 'Installation instructions?',
+        name: 'installation',
+     },
+     {
+        type: 'input',
+        message: 'What is the usage information?',
+        name: 'usage',
+     },
+     {
+        type: 'checkbox',
+        message: 'Any licenses?',
+        name: 'licenses',
+        choices: ['placeholder', 'See the repo for license information.'],
+     },
+     {
+        type: 'input',
+        message: 'Did you have any contributors?',
+        name: 'contributors',
+     },
+     {
+        type: 'input',
+        message: 'what testing did you do?',
+        name: 'tests',
+     },
+     {
+        type: 'input',
+        message: 'What is your Github Username?',
+        name: 'github',
+     },
+     {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+     },
+     {
+        type: 'input',
+        message: 'Any other details for contacting you?',
+        name: 'addInfo',
+     }];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            const newReadme = markdown.generateMarkdown(answers);
+            writeToFile('README.md', newReadme);
+        })
+}
 
 // Function call to initialize app
 init();
